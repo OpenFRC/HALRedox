013661d582998a566c78f13a42f27949fa433d28
Nicholas Burrell <zero_frost@protonmail.com>
2017-05-16 21:52:43 -0700

    Fixed typo in build.rs

--- build.rs
+++ build.rs
@@ -9,10 +9,10 @@ fn main() {
             println!("cargo:rustc-link-lib=dylib={}", lib);
         }
         Command::new("make").arg("-C").arg("cpp/wrapper").arg("arm").output();
-        
+
         println!("cargo:rustc-link-search=native=lib/ni-libraries");
         println!("cargo:rustc-link-search=native=lib/ni-wrapper");
 
-        println!("cargo:rustc-link-lib=static=ni_wrapper.a")
+        println!("cargo:rustc-link-lib=static=ni-wrapper.a")
     }
 }
